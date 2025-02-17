e144784580c0606a9ccb91aaece699d68778d284
Celso Ducao jr <cducaojr@gmail.com>
2025-02-17 19:18:57 +0800

    Update Tag handlers

---
+++ src/Point.Core.Application/Validators/UpdateTagValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using Point.Core.Application.Handlers;
+
+namespace Point.Core.Application.Validators
+{
+    public class UpdateTagValidator : AbstractValidator<UpdateTagRequest>
+    {
+        public UpdateTagValidator() 
+        {
+            RuleFor(x => x.Id)
+                .GreaterThanOrEqualTo(1);
+
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .MaximumLength(30);
+        }
+    }
+}
