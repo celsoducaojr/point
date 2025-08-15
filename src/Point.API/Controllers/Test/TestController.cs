88cfa48fae29ba38ada7a52a34496fa1b432b5ea
Celso Ducao Jr <cducaojr@gmail.com>
2025-08-15 19:50:28 +0800

    Add TestController

---
+++ src/Point.API/Controllers/Test/TestController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Point.API.Controllers.Base;
+using Point.Core.Application.Contracts;
+
+namespace Point.API.Controllers.Test
+{
+    [Route("api/v{version:apiversion}/test")]
+    public class TestController(IPointDbContext pointDbContext) : BaseController
+    {
+        private readonly IPointDbContext _pointDbContext = pointDbContext;
+
+        [HttpGet("live")]
+        public IActionResult IsLive()
+        {
+            return Ok(new { Status = "live" });
+        }
+
+        [HttpGet("ready")]
+        public IActionResult IsReady()
+        {
+            try
+            {
+                var context = _pointDbContext as DbContext;
+                return Ok(new { DatabaseConnected = context.Database.CanConnect() });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { DatabaseConnected = false, Error = ex.Message });
+            }
+        }
+    }
+}
